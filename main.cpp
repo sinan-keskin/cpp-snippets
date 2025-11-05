0097ea3a6a7570e8987c593f7a5138e58aa4f1e9
sinan-keskin <snn.kskn77@gmail.com>
2025-11-05 16:40:55 +0300

    feat: expand C++ sample

--- main.cpp
+++ main.cpp
@@ -1,2 +1,7 @@
 #include <iostream>
-int main(){std::cout<<"Hello from C++ ⚙️"<<std::endl;}
+#include <vector>
+int main(){
+    std::vector<int> v{1,2,3};
+    for(int x: v){ std::cout << "C++ ⚙️ " << x << std::endl; }
+    return 0;
+}
